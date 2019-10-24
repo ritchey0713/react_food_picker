f6aa98372ab05fa57a604da55911460a5cddde0c
Dj <ritchey0713@gmail.com>
2019-10-24 12:15:04 -0400

    clear file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
-console.log('text')
+"use strict";
+
+console.log('app.js loaded');
+
+var template = React.createElement(
+  "p",
+  null,
+  "are we loading JSX?"
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
