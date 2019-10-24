a57dc0f3402944c17bdd23633c74c1dc7be9af2d
Dj <ritchey0713@gmail.com>
2019-10-24 13:40:36 -0400

    add basic templates

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 console.log('app.js loaded')
 
-let template = <p>are we loading JSX?</p>;
+let template = (
+    <div>
+        <h1>Food Picker</h1> 
+        <p>this is my info</p>
+    </div>
+);
+
+let templateTwo = (
+    <div>
+        <h1>Dj</h1>
+        <p>Location: Ohio</p>
+    </div>
+)
 
 let appRoot = document.getElementById("app")
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
 
