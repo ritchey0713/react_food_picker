c4b19ca01b5c73108e55136878535919900914ec
Dj <ritchey0713@gmail.com>
2019-10-31 12:05:57 -0400

    add test logs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("app runs!")
+import React from 'react';
+import ReactDOM from 'react-dom'
+import validator from 'validator';
+
+console.log("VAL",validator.isEmail("test"))
+
+
+const template = <p>Hello</p>
+
+ReactDOM.render(template , document.getElementById("app"))
