ff39dd91a6490c95bd661448605e27392c50e7fe
Dj <ritchey0713@gmail.com>
2019-10-31 12:49:40 -0400

    add option comp

---
+++ src/components/Options.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Option from './Option'
+
+const Options = (props) => {
+  return (
+    <div>
+        <button onClick={props.handleDeleteOptions}>Remove all options!</button>
+        {
+          props.options.length < 1 && <p>Please add some restaurants!</p>
+        }
+        {
+          props.options.map((option) => {
+            return <Option
+               key={option} 
+               optionText={option} 
+               handledeleteOption={props.handledeleteOption} 
+               />
+          })
+        }
+        {/* 
+          without constructor
+          <button onClick={this.handleRemoveAll.bind(this.props)}>Remove all options!</button>  */
+        }
+      </div>
+  )
+}
+
+export default Options
