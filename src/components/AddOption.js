85c8308020b9b4e4855c3fca191047522e05c3ed
Dj <ritchey0713@gmail.com>
2019-10-31 14:48:39 -0400

    remove constructor

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
-  constructor(props){
-    super(props)
-    // use of this inside func, must bind this
-    this.handleFormSubmit = this.handleFormSubmit.bind(this)
-    this.state = {
-      error: undefined
-    }
+  state = {
+    error: undefined
   }
   
-  handleFormSubmit(e) {
+  handleFormSubmit = (e) => {
     e.preventDefault();
     let data = e.target.elements.option.value.trim()
     const error = this.props.handleAddOption(data)
