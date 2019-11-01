5bcfa2034fba3923e4d0e0b14b421851cbf8091e
Dj <ritchey0713@gmail.com>
2019-11-01 10:46:17 -0400

    add class

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <div>
+    <div className="header">
         <h1> {props.title} </h1>
         {
           props.subtitle && <h2> {props.subtitle} </h2>
