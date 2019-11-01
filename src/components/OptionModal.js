af3bdbce1f40ab9551ccf2ba97aae07f6c9efc65
Dj <ritchey0713@gmail.com>
2019-11-01 10:46:13 -0400

    build modals

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -9,8 +9,11 @@ const OptionModal = (props) => {
     <Modal
       isOpen={!!props.selectedOption}
       contentLabel="Selected Restaurant"
+      onRequestClose={props.handleModalClose}
     >
       <h3>Selected Restaurant:</h3>
+      {props.selectedOption && <p>{props.selectedOption}</p>}
+      <button onClick={props.handleModalClose}>Okay!</button>
     </Modal>
   )
 }
