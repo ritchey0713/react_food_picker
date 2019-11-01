8b0235312ddf6ad5db491b47a16907e552ac2db7
Dj <ritchey0713@gmail.com>
2019-11-01 11:54:43 -0400

    import normalize

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import validator from 'validator';
-import FoodPickerApp from './components/FoodPickerApp'
-import "./styles/styles.scss"
+import FoodPickerApp from './components/FoodPickerApp';
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
 ReactDOM.render(<FoodPickerApp/>, document.getElementById("app"))
 
