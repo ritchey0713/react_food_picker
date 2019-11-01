63576d8d22e7d800cb0ece8d3dfab16d1455f22c
Dj <ritchey0713@gmail.com>
2019-11-01 10:46:32 -0400

    import styles

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import validator from 'validator';
 import FoodPickerApp from './components/FoodPickerApp'
+import "./styles/styles.scss"
 
 ReactDOM.render(<FoodPickerApp/>, document.getElementById("app"))
 
