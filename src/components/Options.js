340b617f710a6a285806b1f640742052f4f0d128
Dj <ritchey0713@gmail.com>
2019-11-04 13:19:58 -0500

    add classes

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,29 +4,34 @@ import Option from './Option'
 const Options = (props) => {
   return (
     <div>
+      <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>
         <button
-         className="button--link"
-         onClick={props.handleDeleteOptions}
+          className="button--link"
+          onClick={props.handleDeleteOptions}
         >
-         Remove all options!
+          Remove all options!
         </button>
-        {
-          props.options.length < 1 && <p>Please add some restaurants!</p>
-        }
-        {
-          props.options.map((option) => {
-            return <Option
-               key={option} 
-               optionText={option} 
-               handledeleteOption={props.handledeleteOption} 
-               />
-          })
-        }
-        {/* 
-          without constructor
-          <button onClick={this.handleRemoveAll.bind(this.props)}>Remove all options!</button>  */
-        }
       </div>
+      
+      {
+        props.options.length < 1 && <p className="widget__message">Please add some restaurants!</p>
+      }
+      {
+        props.options.map((option, index) => {
+          return <Option
+              key={option} 
+              optionText={option}
+              count={index + 1} 
+              handledeleteOption={props.handledeleteOption} 
+              />
+        })
+      }
+      {/* 
+        without constructor
+        <button onClick={this.handleRemoveAll.bind(this.props)}>Remove all options!</button>  */
+      }
+    </div>
   )
 }
 
